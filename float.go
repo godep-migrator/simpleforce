749934d24f9f51282e722920c2983211611c5ab7
Jake Basile <jake@jakebasile.com>
2013-12-07 12:32:23 -0600

    corrected docs in float.go.

--- float.go
+++ float.go
@@ -5,49 +5,49 @@ import (
 	"fmt"
 )
 
-// Creates an '=' clause for a int value.
+// Creates an '=' clause for a float value.
 func (c Constraint) EqualsFloat(right float64) Constraint {
 	c.op = "="
 	c.right = fmt.Sprintf("%v", right)
 	return c
 }
 
-// Creates a '<>' clause for a int value.
+// Creates a '<>' clause for a float value.
 func (c Constraint) NotEqualsFloat(right float64) Constraint {
 	c.op = "<>"
 	c.right = fmt.Sprintf("%v", right)
 	return c
 }
 
-// Creates a '>' clause for a int value.
+// Creates a '>' clause for a float value.
 func (c Constraint) GreaterFloat(right float64) Constraint {
 	c.op = ">"
 	c.right = fmt.Sprintf("%v", right)
 	return c
 }
 
-// Creates a '>=' clause for a int value.
+// Creates a '>=' clause for a float value.
 func (c Constraint) GreaterEqualsFloat(right float64) Constraint {
 	c.op = ">="
 	c.right = fmt.Sprintf("%v", right)
 	return c
 }
 
-// Creates a '<' clause for a int value.
+// Creates a '<' clause for a float value.
 func (c Constraint) LessFloat(right float64) Constraint {
 	c.op = "<"
 	c.right = fmt.Sprintf("%v", right)
 	return c
 }
 
-// Creates a '<=' clause for a int value.
+// Creates a '<=' clause for a float value.
 func (c Constraint) LessEqualsFloat(right float64) Constraint {
 	c.op = "<="
 	c.right = fmt.Sprintf("%v", right)
 	return c
 }
 
-// Creates an IN clause for a int value.
+// Creates an IN clause for a float value.
 func (c Constraint) InFloat(in ...float64) Constraint {
 	c.op = " IN "
 	buf := bytes.NewBufferString("(")
@@ -62,7 +62,7 @@ func (c Constraint) InFloat(in ...float64) Constraint {
 	return c
 }
 
-// Creates a NOT IN clause for a int value.
+// Creates a NOT IN clause for a float value.
 func (c Constraint) NotInFloat(in ...float64) Constraint {
 	c.op = " NOT IN "
 	buf := bytes.NewBufferString("(")
